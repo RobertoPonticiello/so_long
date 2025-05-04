70940f0ff6106162f58cc0a0dcf4f2443a83b5da
Roberto nicola Ponticiello <rpontici@e3r8p3.42roma.it>
2025-05-04 17:48:21 +0200

    ADD: Aggiunto get_next_line

--- src/map_validation_1.c
+++ src/map_validation_1.c
@@ -2,8 +2,48 @@
 
 int	error_message(char *msg)
 {
-	ft_putstr_fd("Error\n", 2);
+	ft_putstr_fd("SBAGLIATO!! ", 2);
 	ft_putstr_fd(msg, 2);
 	ft_putstr_fd("\n", 2);
 	return (0);
+}
+
+int	check_extension(char *filename)
+{
+	char	*dot;
+
+	dot = ft_strrchr(filename, '.');
+	if (!dot || dot == filename)
+		return (0);
+	return (ft_strcmp(dot, ".ber") == 0);
+}
+
+int	read_map_line(t_map *map, int fd)
+{
+	char	*line;
+	char	**temp;
+	int		i;
+
+	line = get_next_line(fd);
+	if (!line)
+		return (0);
+	if (map->width == 0)
+		map->width = ft_strlen(line) - 1;
+	temp = (char **)malloc(sizeof(char *) * (map->height + 2)); //MA DIO CANE MALEDETTO 
+	if (!temp)
+		return (0);
+	i = 0;
+	while (i < map->height)
+	{
+		temp[i] = map->grid[i];
+		i++;
+	}
+	line[ft_strlen(line) - 1] = '\0';
+	temp[i] = ft_strdup(line);
+	temp[i + 1] = NULL;
+	free(line);
+	free(map->grid);
+	map->grid = temp;
+	map->height++;
+	return (1);
 }
--- src/so_long.h
+++ src/so_long.h
@@ -3,7 +3,14 @@
 
 # include <mlx.h>
 # include <fcntl.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
 # include "libft.h"
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 1024
+# endif
 
 typedef struct	s_data {
 	void	*img;
@@ -36,5 +43,31 @@ int		check_reachable(char **filled_map, t_map *original);
 int		validate_map(t_map *map, char *filename);
 int		error_message(char *msg);
 void	free_temp_map(char **map, int height);
+
+//GET NEXT LINE
+typedef struct s_chunk
+{
+	char			*data;
+	size_t			size;
+	struct s_chunk	*next;
+}	t_chunk;
+
+typedef struct s_fdata
+{
+	char	buffer[BUFFER_SIZE];
+	size_t	start;
+	size_t	end;
+	t_chunk	*head;
+	t_chunk	*tail;
+}	t_fdata;
+
+char	*get_next_line(int fd);
+t_fdata	*ft_newindex(void);
+t_fdata	*ft_updateindex(t_fdata *index, int fd);
+int		ft_read_and_collect(t_fdata *index, int *found_newline);
+char	*ft_join_chunks(t_fdata *index);
+void	ft_free_chunks(t_fdata *index);
+void	ft_add_chunk(t_fdata *index, const char *src, size_t len);
+void	*ft_memcpy(void *dst, const void *src, size_t n);
 
 #endif
